1425e1ee1309307a840121285d96b5d7d7d0b04b
unimonkiez <unimonkiez@gmail.com>
2016-10-12 01:19:14 +0300

    Working power

--- app/container/app.jsx
+++ app/container/app.jsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => (
-  <div>
-    HELLO im an app
-  </div>
-);
+export default class App extends Component {
+  static handlePowerPress() {
+    if (__DEVSERVER__) {
+      console.log('power press!');
+    } else {
+      fetch('/power', {
+        method: 'POST'
+      });
+    }
+  }
+  static handlePowerRelease() {
+    if (__DEVSERVER__) {
+      console.log('power release!');
+    } else {
+      fetch('/power', {
+        method: 'DELETE'
+      });
+    }
+  }
+  render() {
+    return (
+      <div>
+        HELLO im an app
+        <button onTouchStart={App.handlePowerPress} onMouseDown={App.handlePowerPress} onTouchEnd={App.handlePowerRelease} onMouseUp={App.handlePowerRelease}>
+          Power
+        </button>
+      </div>
+    );
+  }
+}
--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const path = require('path');
 const express = require('express');
+const controller = require('./controller.js');
 
 const app = express();
 const PORT = 80;
 const appPath = path.join(__dirname, 'dist');
 
 app.use(express.static(appPath));
+
+let releasePower;
+// Press
+app.post('/power', (req, res) => {
+  releasePower = controller.pressPower();
+  res.status(200).end();
+});
+
+// Release
+app.delete('/power', (req, res) => {
+  if (releasePower) {
+    releasePower();
+    releasePower = undefined;
+    res.status(200).end();
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`App started on port ${PORT}!`);
